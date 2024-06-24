b1abec6f8f8996f0360ee6bec11b3900136f14ea
santigiamba <112349844+santigiamba@users.noreply.github.com>
2024-06-24 10:52:09 -0300

    casi hecho

---
+++ prueba react/my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductList from './components/ProductList';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProductList />
+    </div>
+  );
+};
+
+export default App;
